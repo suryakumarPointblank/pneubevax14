16d0c851379d80e4b2e235b309589ffa34e829b8
suryakumar2 <surya@pointblank.co.in>
2026-01-17 01:42:53 +0530

    initial changes in homepage for mobile

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <BackgroundSection imageSrc="/homepage/bg_homepage.png">
       {/* Page 1 - Homepage content */}
+      <img
+        className="select-button"
+        src="/homepage/select_button.png"
+        alt="Select Your Team"
+      />
     </BackgroundSection>
   );
 }
