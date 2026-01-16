b0d854c4901a88fcce599aeb09fd18a9f756021e
suryakumar2 <surya@pointblank.co.in>
2026-01-17 02:07:11 +0530

    Homepage for mobiels

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,27 @@
 import BackgroundSection from "@/components/BackgroundSection";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <BackgroundSection imageSrc="/homepage/bg_homepage.png">
       {/* Page 1 - Homepage content */}
       <img
-        className="select-button"
-        src="/homepage/select_button.png"
-        alt="Select Your Team"
+        className="sub-image sub1"
+        src="/homepage/SUB1.png"
+        alt="Sub 1"
       />
+      <img
+        className="sub-image sub2"
+        src="/homepage/sub2.png"
+        alt="Sub 2"
+      />
+      <Link href="/formcollection">
+        <img
+          className="select-button"
+          src="/homepage/select_button.png"
+          alt="Select Your Team"
+        />
+      </Link>
     </BackgroundSection>
   );
 }
